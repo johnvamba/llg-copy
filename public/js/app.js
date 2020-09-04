0bc0de933db14913cfc309d0ab44afa1ccfc7698
Norman <n_amaro@logicbaseinteractive.com>
2020-09-04 16:38:18 +0800

    remove icon

--- public/js/app.js
+++ public/js/app.js
@@ -71836,21 +71836,6 @@ var TextInput = function TextInput(_ref) {
     type: type || "text",
     placeholder: placeholder,
     className: "w-full border-b border-t-0 border-l-0 border-r-0 border-grey-dark focus:outline-none"
-  })), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("svg", {
-    xmlns: "http://www.w3.org/2000/svg",
-    fill: "none",
-    viewBox: "0 0 24 24",
-    stroke: "currentColor"
-  }, /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("path", {
-    "stroke-linecap": "round",
-    "stroke-linejoin": "round",
-    "stroke-width": "2",
-    d: "M15 12a3 3 0 11-6 0 3 3 0 016 0z"
-  }), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("path", {
-    "stroke-linecap": "round",
-    "stroke-linejoin": "round",
-    "stroke-width": "2",
-    d: "M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"
   })));
 };
 
