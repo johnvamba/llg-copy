4070fc1a3109c537e0399ccdeb6c057657e60124
logicbasesabana <72718084+logicbasesabana@users.noreply.github.com>
2020-10-20 13:12:12 +0800

    icons for stories

---
+++ resources/js/svg/stories-empty-drafts.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const StoriesEmptyDraftsIcon = (props)=>{
+	return (<svg width="178" height="178" viewBox="0 0 178 178" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <circle cx="89" cy="89" r="89" fill="#EFF0F2"/>
+                <rect x="44" y="57.0488" width="90.2549" height="32.0053" rx="4" fill="#BEC6CB"/>
+                <path d="M44 126.18V62.1697H71.5245L75.6852 68.5707H134.255V126.18H44Z" fill="black" fill-opacity="0.04"/>
+                <path d="M44 131.301V67.2905H71.5245L75.6852 73.6916H134.255V131.301H44Z" fill="#D3D9DD"/>
+                <circle cx="76.3254" cy="101.536" r="3.52058" fill="#E9ECEE"/>
+                <circle cx="89.1275" cy="101.536" r="3.52058" fill="#E9ECEE"/>
+                <circle cx="101.93" cy="101.536" r="3.52058" fill="#E9ECEE"/>
+                <rect x="88.2097" y="40.5" width="2.5" height="8" rx="1.25" fill="#BEC6CB"/>
+                <rect x="79" y="43.8159" width="2.5" height="6.625" rx="1.25" transform="rotate(-32.5104 79 43.8159)" fill="#BEC6CB"/>
+                <rect width="2.5" height="6.625" rx="1.25" transform="matrix(-0.843294 -0.537453 -0.537453 0.843294 99.9194 43.8159)" fill="#BEC6CB"/>
+            </svg>
+	)
+}
+export default StoriesEmptyDraftsIcon;
