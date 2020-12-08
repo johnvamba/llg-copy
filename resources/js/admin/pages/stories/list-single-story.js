1f7027345d391fd0818c6b7a7cd33949ac64ca12
John Amba <logicbase.amba@gmail.com>
2020-12-08 15:46:23 +0800

    Update: Circlet photo from organization photo

--- resources/js/admin/pages/stories/list-single-story.js
+++ resources/js/admin/pages/stories/list-single-story.js
@@ -10,14 +10,14 @@ const ListSingleStory = ({ set = [], handleForm }) => {
             set.length > 0 && set.map((i, k)=>
                 <li key={k} onClick={() => handleForm(i, false, true)}>
                     <div className="list-single-story__header">
-                        <div className="list-single-story__rounded-img"></div>
+                        <div className="list-single-story__rounded-img" style={{backgroundImage: `url(${i.org_photo})`, backgroundSize: 'cover', backgroundRepeat: 'no-repeat', backgroundPosition: 'center center'}} ></div>
                         <div style={{width: '300px'}}>
                             <h2>{i.title || 'Missing-title'}</h2>
                             <span>{i.date || 'Missing-date'}</span>
                         </div>
                     </div>
                     <div className="list-single-story__body">
-                        <div className="bg-cover bg-center"  style={{backgroundImage: `url(${i.photo})`, backgroundSize: 'cover', backgroundRepeat: 'no-repeat', backgroundPosition: 'center center'}} ></div>
+                        <div className="bg-cover bg-center" style={{backgroundImage: `url(${i.photo})`, backgroundSize: 'cover', backgroundRepeat: 'no-repeat', backgroundPosition: 'center center'}} ></div>
                         <div className="list-single-story__content">
                             <p>{i.short_description || i.description || ''}</p>
                             <div className="list-single-story__footer">
