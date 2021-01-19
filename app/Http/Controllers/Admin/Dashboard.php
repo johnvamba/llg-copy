f85fcbca3c931a71a9bb1d70b9c32f2381e3d618
John Amba <logicbase.amba@gmail.com>
2021-01-19 08:51:06 +0800

    Update: Printable table of needs

--- app/Http/Controllers/Admin/Dashboard.php
+++ app/Http/Controllers/Admin/Dashboard.php
@@ -4,8 +4,45 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-
+// use App\Organization;
+use App\Campus;
+use App\Need;
+use Carbon\Carbon;
 class Dashboard extends Controller
 {
-    //
+    public function __invoke(Request $request)
+    {
+    	//Setup
+    	$query = Need::with(['type', 'organization']);
+    	$startDate = $request->get('startdate') ? Carbon::parse($request->get('startdate'))->startOfDay() : null; 
+		$endDate = $request->get('enddate') ? Carbon::parse($request->get('enddate'))->endOfDay() : null;
+		//If need is still open.
+    	if($request->get('open') == 'true'){
+    		$query->where(function($need) {
+    			$need->whereRaw('raised < goal')->whereNotNull('approved_at');
+    		});
+    	}
+    	//if need has been met
+    	if($request->get('mets') == 'true'){
+    		$query->where(function($need){
+    			$need->whereRaw('raised >= goal')->whereNotNull('approved_at');
+    		});
+    	}
+    	//if in types
+    	if($set = $request->only('donations', 'fundraise', 'volunteer')) {
+    		$keys = array_map(fn($f) => ucfirst($f), array_keys( array_filter($set, fn($i) => $i == 'true') ) );
+    		$query->whereHas('type', fn($type) => $type->whereIn('needs_types.name', $keys) );
+    	}
+
+    	if($startDate && !$endDate)
+    		$query->whereDate('created_at', '>', $startDate);
+    	else if(!$startDate && $endDate)
+    		$query->whereDate('created_at', '<=', $endDate);
+    	else if($startDate && $endDate)
+    		$query->whereBetween('created_at', [$startDate, $endDate]);
+
+    	$needs = $query->latest()->get();
+
+    	return view('template.report', compact('needs'));
+    }
 }
