f034100e32b745a6cfbe009e28f12057dda54b11
Norman <n_amaro@logicbaseinteractive.com>
2021-02-24 16:20:27 +0800

    fcm package, group invitation push notification event

--- app/Listeners/SendInvitationNotification.php
+++ app/Listeners/SendInvitationNotification.php
@@ -5,6 +5,11 @@ namespace App\Listeners;
 use App\Events\GroupInvite;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
+use LaravelFCM\Message\OptionsBuilder;
+use LaravelFCM\Message\PayloadDataBuilder;
+use LaravelFCM\Message\PayloadNotificationBuilder;
+use App\Device;
+use FCM;
 
 class SendInvitationNotification
 {
@@ -27,6 +32,48 @@ class SendInvitationNotification
     public function handle(GroupInvite $event)
     {
         //
-        dd($event);
+        $tokens = Device::where('user_id', $event->params['to'])
+            ->pluck('fcm_token');
+        
+        if (!$tokens->count())
+            return;
+
+        $notification = [
+            "tokens" => $tokens->toArray(),
+            "data" => [
+                'message' => $event->params['description']
+            ]
+        ];
+
+        $this->sendNotification($notification);
+    }
+
+    public function sendNotification($notification)
+    {
+        $optionBuilder = new OptionsBuilder();
+        $optionBuilder->setTimeToLive(60*20);
+
+        $notificationBuilder = new PayloadNotificationBuilder('Group Invitation');
+        $notificationBuilder->setBody($notification['data']['message'])
+                            ->setSound('default');
+
+        $dataBuilder = new PayloadDataBuilder();
+        $dataBuilder->addData(['data' => json_encode($notification['data'], true)]);
+
+        $option = $optionBuilder->build();
+        $notificationBuild = $notificationBuilder->build();
+        $data = $dataBuilder->build();
+
+        $downstreamResponse = FCM::sendTo($notification['tokens'], $option, $notificationBuild, $data);
+
+        return;
+    }
+
+    /** 
+     * Handle a job failure.
+    */
+    public function failed(GroupInvite $event, $exception)
+    {
+        \App::Log("failed group invitation notification");
     }
 }
