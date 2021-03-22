d95ce9a26d00a6531b675a3dba6ae866a66813ee
John Amba <logicbase.amba@gmail.com>
2021-03-22 15:44:02 +0800

    Update: new email templates with responses on every trigger

---
+++ app/Mail/NeedApproved.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+use App\Need;
+
+class NeedApproved extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    // protected $user;
+    protected $need;
+
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct(Need $need)
+    {
+        $this->need = $need;
+    }
+
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this
+            ->subject("Your Need Request has been approved!")
+            ->view('email.need_approve')
+            ->with([
+                'need' => $this->need,
+                'url' => '',
+                'user' => $this->to
+            ]);
+    }
+}
---
+++ app/Mail/OrgAccepted.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+use App\Organization;
+
+class OrgAccepted extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    // protected $user;
+    protected $org;
+
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct(Organization $org)
+    {
+        $this->org = $org;
+    }
+
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this
+            ->subject("You have been approved!")
+            ->view('email.org_approved')
+            ->with([
+                'org' => $this->org,
+                'url' => '',
+                'user' => $this->to
+            ]);
+    }
+}
