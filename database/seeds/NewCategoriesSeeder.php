e391575e21328846d8aa8a15a62a7bbd388dcd2e
John Amba <logicbase.amba@gmail.com>
2021-05-19 10:53:49 +0800

    Working new categories. run
    php artisan db:seed --class=NewCategoriesSeeder

--- database/seeds/NewCategoriesSeeder.php
+++ database/seeds/NewCategoriesSeeder.php
@@ -13,10 +13,10 @@ class NewCategoriesSeeder extends Seeder
     public function run()
     {
         //Monetary
-        \App\NeedsCategory::create(['name' => 'Prison', 'icon' => env('APP_URL').'/assets/icons/Prison.png']);
-        \App\NeedsCategory::create(['name' => 'Seniors', 'icon' => env('APP_URL').'/assets/icons/Senior.png']);
-        \App\NeedsCategory::create(['name' => 'Migrants', 'icon' => env('APP_URL').'/assets/icons/Refugee.png']);
-        \App\NeedsCategory::create(['name' => 'Homeless', 'icon' => env('APP_URL').'/assets/icons/Homeless.png']);
-        \App\NeedsCategory::create(['name' => 'Fostercare', 'icon' => env('APP_URL').'/assets/icons/Fostercare.png']);
+        // \App\NeedsCategory::create(['name' => 'Prison', 'icon' => env('APP_URL').'/assets/icons/Prison.png']);
+        // \App\NeedsCategory::create(['name' => 'Seniors', 'icon' => env('APP_URL').'/assets/icons/Senior.png']);
+        // \App\NeedsCategory::create(['name' => 'Migrants', 'icon' => env('APP_URL').'/assets/icons/Refugee.png']);
+        \App\NeedsCategory::firstOrCreate(['name' => 'Mentoring', 'icon' => env('APP_URL').'/assets/icons/Mentoring.png']);
+        \App\NeedsCategory::firstOrCreate(['name' => 'Admin', 'icon' => env('APP_URL').'/assets/icons/Admin.png']);
     }
 }
