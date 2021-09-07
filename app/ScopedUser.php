9ec2c79f13bf8c3a294b47cd8e06a46702b38c0d
John Amba <logicbase.amba@gmail.com>
2021-09-07 08:56:36 +0800

    User scoping error fixed

--- app/ScopedUser.php
+++ app/ScopedUser.php
@@ -9,4 +9,9 @@ class ScopedUser extends User
     use UserPortalTrait;
 
     protected $table = 'users';
+
+    public function profile()
+    {
+        return $this->hasOne('App\UserProfile', 'user_id');
+    }
 }
